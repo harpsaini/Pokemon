ed0d1c3b657b4d7f406855ff022a58b043dcf3d3
harpsaini <harpreetsinghsn@gmail.com>
2021-10-06 19:04:32 -0400

    added footer and styles

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 
 import './App.css';
 import Search from './Search';
+import Footer from './Footer';
 
 function App() {
   return (
-  <div className="App">
+  <div className="App wrapper">
     <h1>Pokemon</h1>
     <Search/>
+    <Footer/>
   </div>
   )
 }
 
 export default App;
-
-// MVP
-
-// Need a form component to take in user input and pass the information to "PokemonList"
-  //update dom with the pokemons after the GET POKIMON button is clicked
-
-//  create a stateful component 'PokemonList' that takes in user input and queries the api. This component will render returned data from the api response. 
-
-// create a Card component which will get an image and name using props from pokemonlist. 
-
-
-// Stretch goals
-
-// take multiple input values to further sort Pokemons based on Type. 
-// Ability to catch pokemons. 
-
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+return(
+  <footer>
+    <p>Created by Harp Saini at Juno College Of Technology</p>
+    <p>Data from Pokemon Api</p>
+  </footer>
+)
+}
+
+export default Footer
